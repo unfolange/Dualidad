c6df95b0b4c89014587a72838a8501a835a7ee16
unfolange <unfolange@gmail.com>
2025-08-09 18:36:05 -0500

    Avances
    avances

---
+++ Assets/Scripts/EstadoNino.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+public enum EstadoNino
+{
+    Idle = 0,
+    Playing = 1,
+    Studying = 2,
+    Cleaning = 3
+}
+
