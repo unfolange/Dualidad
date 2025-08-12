afb5e86425b2e55f34571669ab4c7cd59a811da6
Cr3ator2012 <efrainrc3@hotmail.com>
2025-08-11 22:23:15 -0500

    Incluir Tutorial
    Incluir tutorial desde el menu

--- Assets/Game/Scripts/MainMenu.cs
+++ Assets/Game/Scripts/MainMenu.cs
@@ -11,7 +11,10 @@ public class MainMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        foreach (ReboteElementoUI boton in Object.FindObjectsByType<ReboteElementoUI>(FindObjectsSortMode.None))
+        {
+            boton.enabled = true;
+        }
     }
 
     // Update is called once per frame
